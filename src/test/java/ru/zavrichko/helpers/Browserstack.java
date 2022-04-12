7136491f72aba8952a7b66a68c0a6febe5bbe62f
a.zavrichko <andrey@zavrichko.dev>
2022-04-12 16:14:37 +0300

    refactoring

--- src/test/java/ru/zavrichko/helpers/Browserstack.java
+++ src/test/java/ru/zavrichko/helpers/Browserstack.java
@@ -1,11 +1,16 @@
 package ru.zavrichko.helpers;
 
+import org.aeonbits.owner.ConfigFactory;
+import ru.zavrichko.config.BrowserstackConfig;
+
 import static io.restassured.RestAssured.given;
 
+
 public class Browserstack {
+    public static BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class);
     public static String videoUrl(String sessionId) {
         return given()
-                .auth().basic("andreyzavrichko_Xe3Bnj", "jhEPjqxKLx9dxaDjMSCk")
+                .auth().basic(config.user(), config.key())
                 .when()
                 .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
                 .then()

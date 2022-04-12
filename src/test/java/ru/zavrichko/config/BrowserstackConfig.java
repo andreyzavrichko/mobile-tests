790bbc09c58a8833ee4eb9386c912a55a5e82b24
a.zavrichko <andrey@zavrichko.dev>
2022-04-12 15:56:34 +0300

    add credentials.properties

--- src/test/java/ru/zavrichko/config/BrowserstackConfig.java
+++ src/test/java/ru/zavrichko/config/BrowserstackConfig.java
@@ -5,12 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.Sources({"classpath:config/browserstack.properties"})
 public interface BrowserstackConfig extends Config {
 
-    @Key("user")
     String user();
-
-    @Key("key")
     String key();
-
-    @Key("app")
     String app();
 }
